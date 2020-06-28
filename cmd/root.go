b95ef1fa8f6099644d1f572035401d54fca52c56
Dylan Meador <dylan.meador@gmail.com>
2020-06-28 11:54:20 -0400

    no help

--- cmd/root.go
+++ cmd/root.go
@@ -34,6 +34,7 @@ func Cmd(s *discordgo.Session, m *discordgo.MessageCreate) *cobra.Command {
 
 	cmd.AddCommand(airhorn.Cmd())
 	cmd.SetHelpCommand(&cobra.Command{Use: "nope", Hidden: true})
+	cmd.Flag("help").Hidden = true
 
 	return cmd
 }
