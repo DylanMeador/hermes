495af0224c9259ffb00ffc18be17a72b467382fc
Dylan Meador <dylan.meador@gmail.com>
2020-06-28 13:54:39 -0400

    Better response

--- cmd/root.go
+++ cmd/root.go
@@ -66,7 +66,7 @@ func Execute(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if err := cmd.ParseFlags(args); err != nil {
 		addReactions(s, m.ChannelID, m.Message.ID, emojis.POOP, emojis.FLAG)
 	} else if _, _, err := cmd.Find(args); err != nil {
-		addReactions(s, m.ChannelID, m.Message.ID, emojis.POOP, emojis.C, emojis.O, emojis.M, emojis.M, emojis.A, emojis.N, emojis.D)
+		addReactions(s, m.ChannelID, m.Message.ID, emojis.POOP, emojis.C, emojis.O, emojis.M, emojis.M2, emojis.A, emojis.N, emojis.D)
 	} else {
 		if err := cmd.ExecuteContext(discord.GenerateDiscordContext(s, m)); err != nil {
 			bug(err, s, m.ChannelID)
